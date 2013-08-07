107a88c2be6df49201a373d04dca889a888ad163
ogranada <oscar.andres.granada@gmail.com>
2013-08-06 20:08:12 -0500

    Initial Commit

---
+++ postgres/views.py
@@ -0,0 +1,14 @@
+# Create your views here.
+
+from django.shortcuts import render_to_response
+
+
+
+
+def index(request):
+    return render_to_response('postgres/index.html')
+
+def backup(request):
+    return render_to_response('postgres/backup.html')
+
+
---
+++ serverAdmin/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'serverAdmin.views.home', name='home'),
+    #url(r'^serverAdmin/', include('serverAdmin.foo.urls')),
+
+    url(r'^$', 'inicio.views.index', name='home'),
+    url(r'^login/$', 'inicio.views.login', name='login'),
+    url(r'^logout/$', 'inicio.views.logout', name='logout'),
+    #################
+    url(r'^pgbackup/$', 'postgres.views.backup', name='login'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
