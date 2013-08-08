7c39b9a55a72074a615c7c404f870cc9f3d8be93
ogranada <oscar.andres.grananda@gmail.com>
2013-08-08 04:06:57 -0500

    changes on postgres app (backup utility)

--- postgres/models.py
+++ postgres/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
 
-# Create your models here.
+class Backup(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Backup"
+
+    date = models.DateTimeField(default=datetime.datetime.today())
+    description = models.TextField(max_length=30)
+    user = models.ForeignKey(User)
+
+
+    def __str__(self):
+    	today = datetime.datetime.now()
+    	date = str(self.date.date().isoformat() if self.date.date()!=today.date() else self.date.time().isoformat().split('.')[0])
+        return self.description+"("+date+")"
+
