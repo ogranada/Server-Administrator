8a8139ae8f0af88274776048681a9959138c897b
ogranada <oscar.andres.granada@gmail.com>
2013-08-15 15:28:27 -0500

    Changes on login and pg_dump interface

--- postgres/pg_utils.py
+++ postgres/pg_utils.py
@@ -3,6 +3,22 @@ import sys
 import os
 import pty
 import time
+import subprocess as sp
+
+
+
+import re
+import subprocess
+
+def is_running(process):
+    s = subprocess.Popen(["ps", "axw"],stdout=subprocess.PIPE)
+    for x in s.stdout:
+        if re.search(process, x):
+            if '<defunct>' in x:
+                return False
+            else:
+                return True
+    return False
 
 def run_pgDump(outfile, hostname, db, user, passwd):
     ## pg_dump consumidores -U letmiapp_user -W -h localhost -f salida.dump
@@ -10,11 +26,15 @@ def run_pgDump(outfile, hostname, db, user, passwd):
     if pid == pty.CHILD:
         os.execlp("pg_dump", "pg_dump", db,"-U", user, "-W", "-h", hostname, "-f", outfile)
     else:
-        os.read(fd, 1024)
+        cn = os.read(fd, 1024)
+        print(cn)
         os.write(fd, passwd + "\n")
-        time.sleep(5)
+        ctd = 0
+        while ctd<120 and is_running(str(pid)):
+            time.sleep(1)
+            print('wait %i...'%(ctd))
+            ctd += 1
         msj = os.read(fd, 1024)
-        print msj
         return msj.strip()=='', msj
 
 
